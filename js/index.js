c6f970f232b7fddd6c62ba65d81ea5c3cd2a464a
Manpreet singh <ms4110415@gmail.com>
2020-10-08 17:30:59 +0530

    Some changes on js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+
+
+
+ function myFun(){
+    const preloader = document.querySelector(".preloader");
+    preloader.style.display = "none";
+}
+
+
+
 const hide = document.querySelector(".hide");
 const ul =  document.querySelector(".navbar");
 const icons =  document.querySelector(".icons");
@@ -12,3 +22,53 @@ hide.addEventListener("click",()=>{
         // icons.style.display = "block";
     }
 });
+
+
+
+const mySkills =  document.getElementById('myskills');
+// console.log(mySkills);
+mySkills.addEventListener("click",()=>{
+    let h1 = document.querySelector("#h1");
+    let h2= document.querySelector("#h2");
+    h1.innerHTML = "HTML5 + <span>CSS3</span> + JS";
+    h2.innerHTML = "<span>Nodejs</span> + Expressjs + <span>Mongodb</span>";
+});
+
+const myproject =  document.getElementById('myproject');
+// console.log(mySkills);
+myproject.addEventListener("click",()=>{
+    let h1 = document.querySelector("#h1");
+    let h2= document.querySelector("#h2");
+    h1.innerHTML = "";
+    h2.innerHTML = "Ecommerce Websites + <span>Web Application</span> + BackEndWeb Application";
+})
+
+const myhobby =  document.getElementById('myhobby');
+// console.log(mySkills);
+myhobby.addEventListener("click",()=>{
+    let h1 = document.querySelector("#h1");
+    let h2= document.querySelector("#h2");
+    h1.innerHTML = "EAT + SLEEP +<SPAN>CODE</SPAN> +REPEAT";
+    h2.innerHTML = "";
+})
+
+const about =  document.getElementById('about');
+// console.log(mySkills);
+about.addEventListener("click",()=>{
+    let h1 = document.querySelector("#h1");
+    let h2= document.querySelector("#h2");
+    h1.innerHTML = "DEVELOPER FROM PUNJAB";
+    h2.innerHTML = "";
+})
+
+const contactme =  document.getElementById('contactme');
+// console.log(mySkills);
+contactme.addEventListener("click",()=>{
+    let h1 = document.querySelector("#h1");
+    let h2= document.querySelector("#h2");
+    h1.innerHTML = "<span>8699594438 On Whatsapp</span>";
+    h2.innerHTML = "<span>rc4329915@gmail.com</span>";
+})
+
+
+
